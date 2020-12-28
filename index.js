fd50814136d7c1a7b0ebf3df45c7a277cadfc604
Michelle Rios <brendamichelle995@gmail.com>
2020-12-28 15:30:46 -0500

    basic game set up complete

---
+++ index.js
@@ -0,0 +1,108 @@
+const button = document.querySelector("#new-game-button")
+
+button.addEventListener('click', (event) => {
+    initiateNewGame();
+});
+
+const getRandomQuoteObject = () => {
+    const quotesArray = [
+        {
+            quote: 'Frankly, my dear, I don’t give a damn.',
+            movie: 'GONE WITH THE WIND',
+            year: 1939
+        },
+        {
+            quote: 'I\'m going to make him an offer he can\'t refuse',
+            movie: 'THE GODFATHER',
+            year: 1972
+        },
+        {
+            quote: `You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.`,
+            movie: 'ON THE WATERFRONT',
+            year: 1954
+        },
+        {
+            quote: `Toto, I've got a feeling we're not in Kansas
+            anymore.`,
+            movie: 'THE WIZARD OF OZ',
+            year: 1939
+        },
+        {
+            quote: `Here's looking at you, kid.`,
+            movie: 'TAXI DRIVER',
+            year: 1976
+        },
+        {
+            quote: `You talking to me?`,
+            movie: 'TAXI DRIVER',
+            year: 1976
+        },
+        {
+            quote: `What we've got here is failure to communicate.`,
+            movie: 'COOL HAND LUKE',
+            year: 1967
+        },
+        {
+            quote: `Show me the money!`,
+            movie: 'JERRY MAGUIRE',
+            year: 1996
+        },
+        {
+            quote: `You can't handle the truth!`,
+            movie: 'A FEW GOOD MEN',
+            year: 1992
+        },
+        {
+            quote: `After all, tomorrow is another day!`,
+            movie: 'GONE WITH THE WIND',
+            year: 1939
+        }];
+
+    return quotesArray[Math.floor(Math.random() * quotesArray.length)];
+};
+
+const createStarterPuzzleDisplay = (quote) => {
+    const quoteLength = quote.length;
+    const puzzleDiv = document.querySelector('#puzzle');
+    const quoteDiv = document.createElement('div')
+    quoteDiv.id = 'quote-div'
+
+    for (let i = 0; i < quoteLength; i++) {
+        const letterSpan = document.createElement('span');
+        letterSpan.dataset.id = i;
+        letterSpan.textContent = /^[A-Z]$/i.test(quote[i]) ? '___ ' : quote[i];
+
+        if (letterSpan.textContent === ' ') {
+            letterSpan.classList.add('space');
+        };
+
+        quoteDiv.append(letterSpan);
+    };
+
+    puzzleDiv.append(quoteDiv);
+};
+
+const addHint = (movieTitle, year) => {
+    const hintDiv = document.querySelector('#hint');
+    const hintPTag = document.createElement('p');
+    hintPTag.textContent = `HINT: ${movieTitle}, ${year}`
+    hintDiv.append(hintPTag)
+}
+
+const clearPreviousGame = () => {
+    const quoteDiv = document.querySelector('div#quote-div')
+    const hintDivPTag = document.querySelector('div#hint p')
+    if(quoteDiv) {
+        quoteDiv.remove();
+        hintDivPTag.remove();
+    };
+}
+
+const initiateNewGame = () => {
+    const movieQuoteObject = getRandomQuoteObject();
+    clearPreviousGame();
+    createStarterPuzzleDisplay(movieQuoteObject.quote);
+    addHint(movieQuoteObject.movie, movieQuoteObject.year);
+};
+
+
