38cb8f311c27c0a40cc11187258ef4a15c65dca6
Michelle Rios <brendamichelle995@gmail.com>
2020-12-29 00:17:28 -0500

    add data

--- data.js
+++ data.js
@@ -1,5 +1,85 @@
 const quotesArray = [
     {
+        quote: `I am serious. And don’t call me Shirley.`,
+        movie: 'AIRPLANE',
+        year: 1980
+    },
+    {
+        quote: `The greatest teacher, failure is.`,
+        movie: 'STAR WARS: THE LAST JEDI',
+        year: 2017
+    },
+    {
+        quote: `We could not talk or talk forever and still find things to not talk about.`,
+        movie: 'BEST IN SHOW',
+        year: 2000
+    },
+    {
+        quote: `I'm the king of the world!`,
+        movie: 'TITANIC',
+        year: 1997
+    },
+    {
+        quote: `Houston, we have a problem.`,
+        movie: 'APOLLO 13',
+        year: 1995
+    },
+    {
+        quote: `To infinity and beyond!`,
+        movie: 'TOY STORY',
+        year: 1995
+    },
+    {
+        quote: `What is this? A center for ants?`,
+        movie: 'ZOOLANDER',
+        year: 2001
+    },
+    {
+        quote: `Sell crazy someplace else. We're all stocked up here.`,
+        movie: 'AS GOOD AS IT GETS',
+        year: 1997
+    },
+    {
+        quote: `She doesn't even go here!`,
+        movie: 'MEAN GIRLS',
+        year: 2004
+    },
+    {
+        quote: `It's not a man purse. It's called a satchel. Indiana Jones wears one.`,
+        movie: 'THE HANGOVER',
+        year: 2009
+    },
+    {
+        quote: `I learned a long time ago that worrying is like a rocking chair. It gives you something to do but it doesn't get you anywhere.`,
+        movie: 'NATIONAL LAMPOON\'S VAN WILDER',
+        year: 2002
+    },
+    {
+        quote: `No, it's a cardigan, but thanks for noticing!`,
+        movie: 'DUMB AND DUMBER',
+        year: 1994
+    },
+    {
+        quote: `I'll have what she's having.`,
+        movie: 'WHEN HARRY MET SALLY',
+        year: 1989
+    },
+    {
+        quote: `We get the warhead and we hold the world ransom for…One million dollars.`,
+        movie: 'AUSTIN POWERS: INTERNATIONAL MAN OF MYSTERY',
+        year: 1997
+    },
+    {
+        quote: `I'm just one stomach flu away from my goal weight.`,
+        movie: 'THE DEVIL WEARS PRADA',
+        year: 2006
+    },
+    {
+        quote: 'This building has to be at least…three times bigger than this!',
+        movie: 'ZOOLANDER',
+        year: 2001
+    },
+    {
         quote: 'Frankly, my dear, I don’t give a damn.',
         movie: 'GONE WITH THE WIND',
         year: 1939
@@ -10,19 +90,14 @@ const quotesArray = [
         year: 1972
     },
     {
-        quote: `You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.`,
-        movie: 'ON THE WATERFRONT',
-        year: 1954
-    },
-    {
         quote: `Toto, I've got a feeling we're not in Kansas anymore.`,
         movie: 'THE WIZARD OF OZ',
         year: 1939
     },
     {
         quote: `Here's looking at you, kid.`,
-        movie: 'TAXI DRIVER',
-        year: 1976
+        movie: 'CASABLANCA',
+        year: 1942
     },
     {
         quote: `You talking to me?`,
@@ -48,4 +123,90 @@ const quotesArray = [
         quote: `After all, tomorrow is another day!`,
         movie: 'GONE WITH THE WIND',
         year: 1939
-    }];
+    },
+    {
+        quote: `Teddy, this is not about you.`,
+        movie: 'Ed from 90 Day Fiance',
+        year: 2020
+    },
+    {
+        quote: `I smell like an egg salad sandwich.`,
+        movie: 'Ed from 90 Day Fiance',
+        year: 2020
+    },
+    {
+        quote: `Am I what you expected?`,
+        movie: 'Ed from 90 Day Fiance',
+        year: 2020
+    },
+    {
+        quote: `I just eat my carrot in the corner.`,
+        movie: 'Natalie from 90 Day Fiance',
+        year: 2020
+    },
+    {
+        quote: `Where is my flowers?`,
+        movie: 'Larissa from 90 Day Fiance',
+        year: 2018
+    },
+    {
+        quote: `Where is my flowers?`,
+        movie: 'Larissa from 90 Day Fiance',
+        year: 2018
+    },
+    {
+        quote: `I don't like Cookie Dough because I think she should lose weight.`,
+        movie: 'Larissa from 90 Day Fiance',
+        year: 2018
+    },
+    {
+        quote: `Debbie is like his second wife, but his mother.`,
+        movie: 'Larissa from 90 Day Fiance',
+        year: 2018
+    },
+    {
+        quote: `Who is against the queen will die!`,
+        movie: 'Larissa from 90 Day Fiance',
+        year: 2018
+    },
+    {
+        quote: `I think I deserve everything!`,
+        movie: 'Anfisa from 90 Day Fiance',
+        year: 2016
+    },
+    {
+        quote: `I am only with you for your money.`,
+        movie: 'Anfisa from 90 Day Fiance',
+        year: 2016
+    },
+    {
+        quote: `Bring me my red bag WITH MY MAKEUP!`,
+        movie: 'Anfisa from 90 Day Fiance',
+        year: 2016
+    },
+    {
+        quote: `You will not disrespect me like this today. Good night.`,
+        movie: 'Chantel from 90 Day Fiance',
+        year: 2017
+    },
+    {
+        quote: `Almost there, Lazy.`,
+        movie: 'Azan from 90 Day Fiance',
+        year: 2016
+    },
+    {
+        quote: `I work so much work.`,
+        movie: 'Zied from 90 Day Fiance',
+        year: 2019
+    },
+    {
+        quote: `Pick your sin and I’ll help you to accommodate it.`,
+        movie: 'Doug from 90 Day Fiance',
+        year: 2019
+    },
+    {
+        quote: `She wants caviar and champagne on a malt liqueur budget.`,
+        movie: 'Doug from 90 Day Fiance',
+        year: 2019
+    },
+];
